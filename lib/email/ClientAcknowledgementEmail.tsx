3ebf008f4e7d5a0f546e6ff1aba8801cea510070
Martin Carroll <martinc343@gmail.com>
2025-12-18 09:47:27 +1000

    Update email footer to use avob-footer-image.jpg instead of AVOB_DF.png

--- lib/email/ClientAcknowledgementEmail.tsx
+++ lib/email/ClientAcknowledgementEmail.tsx
@@ -119,10 +119,10 @@ const ClientAcknowledgementEmail: React.FC<ClientAcknowledgementEmailProps> = ({
 
             <Section style={avobSection}>
               <Img
-                src="https://rockywebstudio.com.au/images/avob/AVOB_DF.png"
+                src="https://rockywebstudio.com.au/images/avob-footer-image.jpg"
                 alt="Australian Veteran Owned Business"
-                width={80}
-                height={80}
+                width={100}
+                height="auto"
                 style={{ display: "inline-block" }}
               />
               <Text style={{ ...footerText, marginTop: 8 }}>
